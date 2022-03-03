f666fdc117a6a6e40a45d29e351883165536eaad
kumaranu <kumaranu@iu.edu>
2022-03-02 20:02:06 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+from qiskit import QuantumCircuit, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram
+from math import gcd
+from numpy.random import randint
+import pandas as pd
+from fractions import Fraction
+print("Imports Successful")
+
+if __name__ == '__main__':
+    print('PyCharm')
+
