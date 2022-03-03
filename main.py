eb29b6df3a89d87b73361fded264e11451cfa94b
kumaranu <kumaranu@iu.edu>
2022-03-03 15:31:57 -0500

    Trying again with a factoring example the next day.

--- main.py
+++ main.py
@@ -45,64 +45,114 @@ def qft_dagger(n):
     qc.name = "QFT†"
     return qc
 
+def a2jmodN(a, j, N):
+    """Compute a^{2^j} (mod N) by repeated squaring"""
+    for i in range(j):
+        a = np.mod(a**2, N)
+    return a
+
+def qpe_amod15(a):
+    n_count = 8
+    qc = QuantumCircuit(4+n_count, n_count)
+    for q in range(n_count):
+        qc.h(q)     # Initialize counting qubits in state |+>
+    qc.x(3+n_count) # And auxiliary register in state |1>
+    for q in range(n_count): # Do controlled-U operations
+        qc.append(c_amod15(a, 2**q),
+                  [q] + [i+n_count for i in range(4)])
+    qc.append(qft_dagger(n_count), range(n_count)) # Do inverse-QFT
+    qc.measure(range(n_count), range(n_count))
+    # Simulate Results
+    aer_sim = Aer.get_backend('aer_simulator')
+    # Setting memory=True below allows us to see a list of each sequential reading
+    t_qc = transpile(qc, aer_sim)
+    qobj = assemble(t_qc, shots=1)
+    result = aer_sim.run(qobj, memory=True).result()
+    readings = result.get_memory()
+    print("Register Reading: " + readings[0])
+    phase = int(readings[0],2)/(2**n_count)
+    print("Corresponding Phase: %f" % phase)
+    return phase
+
+
 # Specify variables
-n_count = 8  # number of counting qubits
+n_count = 5  # number of counting qubits
 a = 13
 
 if __name__ == '__main__':
-    # Create QuantumCircuit with n_count counting qubits
-    # plus 4 qubits for U to act on
-    qc = QuantumCircuit(n_count + 4, n_count)
+    ## Create QuantumCircuit with n_count counting qubits
+    ## plus 4 qubits for U to act on
+    #qc = QuantumCircuit(n_count + 4, n_count)
 
-    # Initialize counting qubits
-    # in state |+>
-    for q in range(n_count):
-        qc.h(q)
+    ## Initialize counting qubits
+    ## in state |+>
+    #for q in range(n_count):
+    #    qc.h(q)
 
-    # And auxiliary register in state |1>
-    qc.x(3+n_count)
+    ## And auxiliary register in state |1>
+    #qc.x(3+n_count)
 
-    # Do controlled-U operations
-    for q in range(n_count):
-        qc.append(c_amod15(a, 2**q),
-                  [q] + [i+n_count for i in range(4)])
+    ## Do controlled-U operations
+    #for q in range(n_count):
+    #    qc.append(c_amod15(a, 2**q),
+    #              [q] + [i+n_count for i in range(4)])
 
-    # Do inverse-QFT
-    qc.append(qft_dagger(n_count), range(n_count))
+    ## Do inverse-QFT
+    #qc.append(qft_dagger(n_count), range(n_count))
 
-    # Measure circuit
-    qc.measure(range(n_count), range(n_count))
-    qc.draw(fold=-1, output='mpl')  # -1 means 'do not fold'
-    #qc.draw(output='mpl', filename='shorsAlgoCircuit.png')  # -1 means 'do not fold'
+    ## Measure circuit
+    #qc.measure(range(n_count), range(n_count))
+    #qc.draw(fold=-1, output='mpl')  # -1 means 'do not fold'
+    ##qc.draw(output='mpl', filename='shorsAlgoCircuit.png')  # -1 means 'do not fold'
 
-    aer_sim = Aer.get_backend('aer_simulator')
-    t_qc = transpile(qc, aer_sim)
-    qobj = assemble(t_qc)
-    results = aer_sim.run(qobj).result()
-    counts = results.get_counts()
-    plot_histogram(counts)
-    plt.show()
+    #aer_sim = Aer.get_backend('aer_simulator')
+    #t_qc = transpile(qc, aer_sim)
+    #qobj = assemble(t_qc)
+    #results = aer_sim.run(qobj).result()
+    #counts = results.get_counts()
+    #plot_histogram(counts)
+    #plt.show()
 
-    rows, measured_phases = [], []
-    for output in counts:
-        decimal = int(output, 2)  # Convert (base 2) string to decimal
-        phase = decimal/(2**n_count)  # Find corresponding eigenvalue
-        measured_phases.append(phase)
-        # Add these values to the rows in our table:
-        rows.append([f"{output}(bin) = {decimal:>3}(dec)",
-                     f"{decimal}/{2**n_count} = {phase:.2f}"])
-    # Print the rows in a table
-    headers=["Register Output", "Phase"]
-    df = pd.DataFrame(rows, columns=headers)
-    print(df)
+    #rows, measured_phases = [], []
+    #for output in counts:
+    #    decimal = int(output, 2)  # Convert (base 2) string to decimal
+    #    phase = decimal/(2**n_count)  # Find corresponding eigenvalue
+    #    measured_phases.append(phase)
+    #    # Add these values to the rows in our table:
+    #    rows.append([f"{output}(bin) = {decimal:>3}(dec)",
+    #                 f"{decimal}/{2**n_count} = {phase:.2f}"])
+    ## Print the rows in a table
+    #headers=["Register Output", "Phase"]
+    #df = pd.DataFrame(rows, columns=headers)
+    #print(df)
 
-    rows = []
-    for phase in measured_phases:
-        frac = Fraction(phase).limit_denominator(15)
-        rows.append([phase, f"{frac.numerator}/{frac.denominator}", frac.denominator])
-    # Print as a table
-    headers=["Phase", "Fraction", "Guess for r"]
-    df = pd.DataFrame(rows, columns=headers)
-    print(df)
+    #rows = []
+    #for phase in measured_phases:
+    #    frac = Fraction(phase).limit_denominator(15)
+    #    rows.append([phase, f"{frac.numerator}/{frac.denominator}", frac.denominator])
+    ## Print as a table
+    #headers=["Phase", "Fraction", "Guess for r"]
+    #df = pd.DataFrame(rows, columns=headers)
+    #print(df)
 
     print("Finished all the steps.")
+    N = 15
+    a = 7
+    factor_found = False
+    attempt = 0
+    while not factor_found:
+        attempt += 1
+        print("\nAttempt %i:" % attempt)
+        phase = qpe_amod15(a) # Phase = s/r
+        frac = Fraction(phase).limit_denominator(N) # Denominator should (hopefully!) tell us r
+        r = frac.denominator
+        print("Result: r = %i" % r)
+        if phase != 0:
+            # Guesses for factors are gcd(x^{r/2} ±1 , 15)
+            guesses = [gcd(a**(r//2)-1, N), gcd(a**(r//2)+1, N)]
+            print("Guessed Factors: %i and %i" % (guesses[0], guesses[1]))
+            for guess in guesses:
+                if guess not in [1,N] and (N % guess) == 0: # Check to see if guess is a factor
+                    print("*** Non-trivial factor found: %i ***" % guess)
+                    factor_found = True
+
