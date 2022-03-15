e0767df9babde09d73b70e1f7077b817538c3a1b
kumaranu <anukumar94663@gmail.com>
2022-03-15 17:01:18 -0400

    Updating to start the work on March 15th 2022.

--- map1.py
+++ map1.py
@@ -1,9 +1,13 @@
 import random
 import numpy as np
+from scipy.fft import fft, ifft
 
 #initialization
 nSteps, nTerms = 5, 3
 v = np.asarray([0.4, 0.1, 0.01])
+x = np.arange(99)
+f1 = np.exp(-(x-1)**2) + np.exp(-x**2)
+f2 = np.exp(-(x-2)**2) + np.exp(-x**2)
 
 #normalization and binning
 normalized_v = v / np.sqrt(np.sum(v**2))
@@ -20,3 +24,5 @@ for i in range(nSteps):
         if n < binList[j]:
             print(n, i, j)
 
+fft_x = fft(f1)
+print(fft_x)
